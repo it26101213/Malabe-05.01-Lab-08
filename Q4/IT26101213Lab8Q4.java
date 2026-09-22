5d0147ffba1ffeacd9256a3bd353963f60fb7977
it26101213 <it26101213@my.sliit.lk>
2026-09-22 21:48:44 +0530

    Add files via upload

---
+++ Q4/IT26101213Lab8Q4.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class IT26101213Lab8Q4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] studentsArray = new int[8];
+        int count = 0;
+
+        while (count < 8) {
+            System.out.print("Enter Student ID for Student " + (count + 1) + ": ");
+            int id = input.nextInt();
+
+            if (id <= 0) {
+                System.out.println("Error: Please Enter ONLY Positive Numbers");
+            } else {
+                studentsArray[count] = id;
+                count++;
+            }
+        }
+
+        System.out.print("\nEnter a Student ID to Search: ");
+        int searchId = input.nextInt();
+
+        boolean found = false;
+        for (int id : studentsArray) {
+            if (id == searchId) {
+                found = true;
+                break;
+            }
+        }
+
+        System.out.println();
+        if (found) {
+            System.out.println("Student is Available");
+        } else {
+            System.out.println("Student is Not Available");
+        }
+    }
+}
